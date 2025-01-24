0465588ac56c31e7808b85a86d9e5b3a9f0c563e
Dulip Sameera <dsgitrepo101@gmail.com>
2025-01-24 14:55:08 +0530

    Add ui elements

--- src/main/java/com/fetchbrowser/app/controller/MainSceneController.java
+++ src/main/java/com/fetchbrowser/app/controller/MainSceneController.java
@@ -1,4 +1,22 @@
 package com.fetchbrowser.app.controller;
 
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebView;
+
 public class MainSceneController {
+    public AnchorPane root;
+    public ImageView imgSearch;
+    public TextField txtSearch;
+    public WebView wbDisplay;
+
+    public void initialize() {
+
+    }
+
+    public void imgSearchOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
 }
