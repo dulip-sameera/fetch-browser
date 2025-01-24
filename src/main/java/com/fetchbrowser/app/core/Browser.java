035ffc8ee8e22a74a3cd7746820c3d06ceb2732e
Dulip Sameera <dsgitrepo101@gmail.com>
2025-01-24 15:43:15 +0530

    Split host from the url

--- src/main/java/com/fetchbrowser/app/core/Browser.java
+++ src/main/java/com/fetchbrowser/app/core/Browser.java
@@ -23,6 +23,27 @@ public class Browser {
             case null, default -> -1;
         };
 
+        //host
+        String urlWithoutProtocol = hasProtocol ? url.substring(i + 3) : url;
+        boolean hasPort = false;
+        int j = -1, k = -1, l = -1;
+        j = urlWithoutProtocol.indexOf(":");
+        if (j != -1) {
+            host = urlWithoutProtocol.substring(0, j);
+            hasPort = true;
+        } else if ((k = urlWithoutProtocol.indexOf("/")) != -1) {
+            host = urlWithoutProtocol.substring(0, k);
+        } else if ((l = urlWithoutProtocol.indexOf("?")) != -1) {
+            host = urlWithoutProtocol.substring(0, l);
+        } else {
+            host = urlWithoutProtocol;
+        }
+
+        System.out.println("Protocol : " + protocol);
+        System.out.println("Host : " + host);
+        System.out.println("Port : " + port);
+//        System.out.println("Path : " + path);
+
         return new String[] { url };
     };
 
